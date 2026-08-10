56b589f14c33bab9784ba32a54d9fe84ae1ee63d
Roshan Raut <rautroshan0206@gmail.com>
2026-08-10 08:06:42 +0000

    Add Changes

---
+++ Basics/Logic/pattern8.java
@@ -0,0 +1,27 @@
+class Pattern {
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * (n - i) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+/*
+OUTPUT:
+*******
+ *****
+  ***
+   *
+ */
